1ca428718b3386905d5ef8ef2a2fa5e444d75a89
Sinath Ahmed <sinatfahad@gmail.com>
2025-04-11 23:11:30 +0600

    a program to check if there are any vowels in a string

---
+++ ostad_Module_12_python_practice.py
@@ -0,0 +1,16 @@
+# 1. Write a program to check if there is a vowel in a string.
+
+names = input("enter: ") #Programming
+vowels = "aeiouAEIOU"
+count = 0
+
+for name in names:
+    for i in vowels:
+        if i == name:
+            count+=1
+
+if count >= 1: 
+    print("The string contains a vowel.")
+else:
+    print("The string does not contain any vowel.")
+
