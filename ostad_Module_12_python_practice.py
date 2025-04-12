72f56bb4852aedb9148169ba9f4e55415d3db196
Sinath Ahmed <sinatfahad@gmail.com>
2025-04-13 00:07:20 +0600

    all question solved from module 12.

--- ostad_Module_12_python_practice.py
+++ ostad_Module_12_python_practice.py
@@ -1,6 +1,6 @@
 # 1. Write a program to check if there is a vowel in a string.
 
-"""names = input("enter: ") #Programming
+names = input("enter: ") 
 vowels = "aeiouAEIOU"
 count = 0
 
@@ -12,7 +12,7 @@ for name in names:
 if count >= 1: 
     print("The string contains a vowel.")
 else:
-    print("The string does not contain any vowel.")"""
+    print("The string does not contain any vowel.")
 
 #2. Write a program where user will give height(height) and weight(kg) and then BMI will be calculated.
 
