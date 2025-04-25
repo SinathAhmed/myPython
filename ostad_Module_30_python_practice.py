ff46261b0b9f882e98f78ef4e32cf37a49603326
Sinath Ahmed <sinatfahad@gmail.com>
2025-04-25 22:33:33 +0600

    added problem 1

--- ostad_Module_30_python_practice.py
+++ ostad_Module_30_python_practice.py
@@ -1,3 +1,12 @@
+#1 Write a program that checks if a year entered by the user is a leap year or not.
+
+# inp = int(input())
+
+# if (inp % 4 == 0 and inp % 100 != 0) or (inp % 400 == 0):
+#     print(f"{inp} is a leap year.")
+# else:
+#     print(f"{inp} is not a leap year.")
+
 #3 Write a program that checks if a number entered by the user is positive, negative, or zero.
 
 # inp = int(input())
@@ -11,13 +20,13 @@
 
 #4 Write a program that swaps the values of two variables.
 
-inp = map(int, input().split()) 
-a, b = inp
-temp = 0
-print(f"Before swapping: num1 = {a}, num2 = {b}")
+# inp = map(int, input().split()) 
+# a, b = inp
+# temp = 0
+# print(f"Before swapping: num1 = {a}, num2 = {b}")
 
-temp = a
-a = b
-b = temp
+# temp = a
+# a = b
+# b = temp
 
-print(f"After swapping: num1 = {a}, num2 = {b}")
+# print(f"After swapping: num1 = {a}, num2 = {b}")
